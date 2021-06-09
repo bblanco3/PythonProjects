b211b852a6db84497fa9081f36cd69029207b912
bblanco3 <47936870+bblanco3@users.noreply.github.com>
2021-06-09 15:36:27 -0500

    Add files via upload

---
+++ folder_spam.py
@@ -0,0 +1,16 @@
+import os
+
+
+
+options = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+root = os.getcwd()
+
+
+for letter1 in options:
+     for letter2 in options:
+          for letter3 in options:
+               for letter4 in options:
+
+                    os.makedirs(os.path.join(root, letter1, letter2, letter3, letter4))
+
+
